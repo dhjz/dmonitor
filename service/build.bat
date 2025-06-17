40ac3529a9cc280d7a8fa1715c4e86b2eafc9a0b
dhjz <909514844@qq.com>
2025-06-17 19:32:29 +0800

    1

--- service/build.bat
+++ service/build.bat
@@ -2,13 +2,16 @@
 ::go build -ldflags "-s -w -H=windowsgui"
 :: go build -o ddns.exe
 ::cp -Force -r ../web/dist/* ./webapp/
-rmdir /S /Q .\webapp
-xcopy /E /Y "..\web\dist\*" ".\webapp\"
+::rmdir /S /Q .\webapp
+::xcopy /E /Y "..\web\dist\*" ".\webapp\"
 
 go env -w GOOS=linux
 go build -ldflags "-s -w" -o ./dist/
 
-go env -w GOOS=windows
+go env -w GOOS=linux GOARCH=arm64
+go build -ldflags "-s -w" -o ./dist/dmonitor_arm64
+
+go env -w GOOS=windows GOARCH=amd64
 go build -ldflags "-s -w -H=windowsgui" -o ./dist/
 :: go build -o ddns.exe
 
